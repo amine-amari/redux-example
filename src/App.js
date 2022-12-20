b5e38f0b2f0eb884a6c681c50abb6a31e10304bd
Amine Amari <amari.mohamed.amine90@gmail.com>
2022-12-20 20:40:37 +0100

    added action and dispatch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { increment, decrement, logIn} from './actions';
 
 function App() {
   
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector (state => state.isLogged);
+  const dispatch = useDispatch();
   
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      {isLogged ? <h3>Valuable Information</h3> : ''}   
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      
+      <div>
+        <button onClick={() => dispatch(logIn())}>Log In</button>
+        {isLogged ? <h3>Valuable Information</h3> : ''} 
+      </div>
+        
     </div>
   );
 }
