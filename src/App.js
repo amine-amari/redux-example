25c83d3d2de4c97d1f1855125895943db125b063
Amine Amari <amari.mohamed.amine90@gmail.com>
2022-12-20 20:29:07 +0100

    access state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {useSelector} from 'react-redux';
 
 function App() {
+  
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector (state => state.isLogged);
+  
   return (
     <div className="App">
-      APP
+      <h1>Counter {counter}</h1>
+      {isLogged ? <h3>Valuable Information</h3> : ''}   
     </div>
   );
 }
